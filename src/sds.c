51bece5c621bb6da9ad1997d8510e809d13903b4
llk_home <daimon@DESKTOP-C0MU4U3>
2023-03-28 16:11:49 +0800

    my_first

--- src/sds.c
+++ src/sds.c
@@ -27,7 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
+//11111
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
